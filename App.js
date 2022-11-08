f8ce28bb2978a71d664f049a26202a6663aac8ba
Fandika <fandika.ikhsan@gmail.com>
2022-11-08 19:32:37 +0700

    feat: encrypt decrypt with jwt

--- App.js
+++ App.js
@@ -3,39 +3,9 @@ import { Text, View, StyleSheet, Button } from "react-native"
 import { BarCodeScanner } from "expo-barcode-scanner"
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(null)
-  const [scanned, setScanned] = useState(false)
-
-  useEffect(() => {
-    const getBarCodeScannerPermissions = async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync()
-      setHasPermission(status === "granted")
-    }
-
-    getBarCodeScannerPermissions()
-  }, [])
-
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true)
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`)
-  }
-
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>
-  }
-
   return (
     <View style={styles.container}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-      {scanned && (
-        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
-      )}
+      <Text style={styles.text}>Homepage</Text>
     </View>
   )
 }
@@ -45,5 +15,9 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
   },
 })
--- services/barcode.js
+++ services/barcode.js
@@ -1,4 +1,22 @@
+// jwt import
+const jwt = require("jsonwebtoken")
+
+// encrypt with jwt
+const encrypt = (data) => {
+  const token = jwt.sign(data, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  })
+  return token
+}
+
+// decrypt with jwt
+const decrypt = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET)
+}
+
 const barcodeParser = (barcode) => {
+  //
+
   return `Your barcode is ${barcode}`
 }
 
