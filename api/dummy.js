cdd869f471eb4fc0fa56cf11d14f9ea17f1ec3d9
Fandika <fandika.ikhsan@gmail.com>
2022-11-08 16:55:40 +0700

    feat: add dummy data ticket

---
+++ api/dummy.js
@@ -0,0 +1,22 @@
+const ticketData = {
+  data: [
+    {
+      ticket_id: 123123,
+      ticket_type: "VIP",
+      ticket_price: 100,
+      ticket_date: "2020-01-01",
+      ticket_time: "12:00",
+      ticket_location: "Helsinki",
+    },
+    {
+      ticket_id: 123124,
+      ticket_type: "VIP",
+      ticket_price: 100,
+      ticket_date: "2020-01-01",
+      ticket_time: "12:00",
+      ticket_location: "Helsinki",
+    },
+  ],
+}
+
+export { ticketData }
